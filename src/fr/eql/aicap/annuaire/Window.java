e6d3cad28fc4fbe4bf2dd20df1b7ff47892cf11f
OtterStrikesBack <ju_morandin@hotmail.fr>
2023-01-13 17:06:11 +0100

    windows in javafx

--- src/fr/eql/aicap/annuaire/Window.java
+++ src/fr/eql/aicap/annuaire/Window.java
@@ -2,88 +2,114 @@ package fr.eql.aicap.annuaire;
 
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.layout.BorderPane;
+import java.io.*;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
 
 public class Window extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
 
 
-        //ListView<String> listViewReference = new ListView<String>();
+        ObservableList<Stagiaires> data = getStagiairesList();
 
-        //adding item to list
-//        listViewReference.getItems().add("Promotion");
-//        listViewReference.getItems().add("Nom");
-//        listViewReference.getItems().add("Prénom");
-//        listViewReference.getItems().add("Année");
-//        listViewReference.getItems().add("Département");
+        //Création Table
+        TableView<Stagiaires> table = new TableView<Stagiaires>();
+        table.setEditable(true);
 
-//        Label promotion = new Label("Promotion");
-//        Label nom = new Label("Nom");
-//        Label prenom = new Label("Prénom");
-//        Label year = new Label("Année");
-//        Label dpt = new Label("Département");
+        //Création des 5colonnes
 
-//        listViewReference.setOrientation(Orientation.HORIZONTAL);
+        TableColumn<Stagiaires, String> promoCol =
+                new TableColumn<Stagiaires, String>("Promotion");
+        promoCol.setMinWidth(100);
 
-        //TextArea liste = new TextArea();
+        TableColumn<Stagiaires, Integer> anneeCol =
+                new TableColumn<Stagiaires, Integer>("Année");
+        anneeCol.setMinWidth(100);
+        TableColumn<Stagiaires, String> nomCol =
+                new TableColumn<Stagiaires, String>("Nom");
+        nomCol.setMinWidth(100);
+        TableColumn<Stagiaires, String> prenomCol =
+                new TableColumn<Stagiaires, String>("Prénom");
+        prenomCol.setMinWidth(100);
+
+        TableColumn<Stagiaires, Integer> dptCol =
+                new TableColumn<Stagiaires, Integer>("Département");
+        dptCol.setMinWidth(100);
+
+        // ajout des colonnes à la table
+
+        table.getColumns().addAll(promoCol,nomCol,prenomCol, anneeCol,dptCol);
+        table.setItems(data);
+
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_LEFT);
         Button button1= new Button("Afficher l'annuaire");
-//        button1.setOnAction(event ->{
-//        try
-//        {
-//            FileReader in = new FileReader("C:\\Users\\Formation\\Documents\\Projects\\Trainees-directory\\stagiaires.txt");
-//            BufferedReader br = new BufferedReader(in);
-//            StringBuffer sb = new StringBuffer();
-//            String line;
-//            while((line = br.readLine()) != null)
-//            {
-//                sb.append(line);
-//                sb.append("\n");
-//            }
-//            in.close();
-//            //liste.setText(sb.toString());
-//
-//        } catch (FileNotFoundException e) {
-//            throw new RuntimeException(e);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        });
+        Button button2= new Button("Exporter en PDF");
+        Button button3= new Button("Rechercher");
 
-       BorderPane root = new BorderPane();
-//        root.getChildren().addAll(promotion, nom, prenom, year, dpt, button1);
-        root.getChildren().addAll(button1);
-        root.setPadding(new Insets(20, 20, 20, 20));
-        //HBox hbox = new HBox(listViewReference);
-        /* creating scene */
-        Scene scene = new Scene(root, 800, 200);
-        /* adding scene to stage */
+        Button button4= new Button("Se connecter");
+        button4.setTranslateX(50);
+        button4.setTranslateY(30);
+
+        hbBtn.getChildren().addAll(button1, button2, button3, button4);
+
+        VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(5,5,5,5));
+        vbox.getChildren().addAll(table);
+        Scene scene = new Scene(vbox);
         stage.setScene(scene);
-        //root.setTop(Label);
-        //root.setCenter(liste);
-        /* display scene for showing output */
         stage.show();
         scene.getStylesheets().add(getClass().getResource("css.css").toExternalForm());
         stage.setTitle("Annuaire SQL");
         stage.setScene(scene);
         stage.show();
     }
+
+    private ObservableList<Stagiaires> getStagiairesList(){
+        Stagiaires stagiaire1 = new Stagiaires("BLOB 56", 2008, "KANAAN", "Suhaila", 94);
+        Stagiaires stagiaire2 = new Stagiaires("BLOB 56", 2008, "FIORE", "Prassede", 94);
+        Stagiaires stagiaire3 = new Stagiaires("BLOB 56", 2008, "BEATRICE", "Rita" ,13);
+        ObservableList<Stagiaires> list = FXCollections.observableArrayList(stagiaire1, stagiaire2, stagiaire3);
+
+        return list;
+
+
+    }
+
+
+
+
+//        button1.setOnAction(event -> {
+//            FileReader in = null;
+//            try {
+//                in = new FileReader("C:\\Users\\Formation\\Documents\\Projects\\Trainees-directory\\stagiaires.txt");
+//            } catch (FileNotFoundException e) {
+//                throw new RuntimeException(e);
+//            }
+//            BufferedReader br = new BufferedReader(in);
+//            StringBuffer sb = new StringBuffer();
+//            String line;
+//
+//
+//        });
+
+
+
+
 
     public static void main(String[] args) {
         Application.launch(args);
