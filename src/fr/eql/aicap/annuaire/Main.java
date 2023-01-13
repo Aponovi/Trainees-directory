40fd9acf69bacf4284d43491e0b0002e8553a819
Aponovi <elfi.krupsky@gmail.com>
2023-01-13 12:02:53 +0100

    clean comments

--- src/fr/eql/aicap/annuaire/Main.java
+++ src/fr/eql/aicap/annuaire/Main.java
@@ -85,40 +85,7 @@ public class Main {
                     }
                     compteurLigne += 1;
                     compteurStagiaire += 1;
-
-//                    String convert = String.format("%0" + Integer.numberOfLeadingZeros(leftChild) + "d", leftChild);
-//
-//                    System.out.println(convert);
-//
-//                    stagiaires.writeInt(Integer.parseInt(convert));
-
-//                System.out.println(leftChild);
-//                System.out.println(Integer.numberOfLeadingZeros(leftChild));
-//                System.out.println(Integer.numberOfLeadingZeros(leftChild) * 0);
-                    // leftChild = (Integer.numberOfLeadingZeros(leftChild) * 0) + leftChild;
-
-
-//                def digitFormatter(long: Long, numDigits: Int): String = {
-//                        val padlength = if (long >= 0) numDigits else numDigits + 1
-//                String.format(s"%0${padlength}d", long)
-//                  }
-                //}
-//            for (int i = 0; i < compteurStagiaire; i++) {
-
-                // System.out.println(compteurStagiaire);
             }
-
-            // ajout du leftchild qui est un integer
-            // stagiaires.writeInt(leftChild);
-            // System.out.println(leftChild);
-            // cherche à compléter le leftchild avec des espaces vides pour "garder sa place"
-
-
-            // stagiaires.writeInt(leftChild,LEFTCHILD);
-            // stagiaires.writeInt(rightChild,LEFTCHILD);
-//            System.out.println("Taille du RAF : " + stagiaires.length());
-//            System.out.println("nb stagiaires : " + compteurStagiaire);
-//            System.out.println("lenght raf par stagiaires : " + (stagiaires.length() / compteurStagiaire));
 
             stagiaires.close();
 
