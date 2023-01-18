9cad9b2f035cab268f356232e1339a6ef61aba05
hmytt <hamiyet.akyazici@gmail.com>
2023-01-18 10:45:07 +0100

    test commit

--- src/fr/eql/aicap/annuaire/Window.java
+++ src/fr/eql/aicap/annuaire/Window.java
@@ -3,6 +3,8 @@ package fr.eql.aicap.annuaire;
 
 import javafx.application.Application;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -11,13 +13,20 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.*;
+import java.util.List;
+
 import javafx.scene.control.*;
 import javafx.collections.FXCollections;
+
+import static fr.eql.aicap.annuaire.Main.BINARYFILE;
+import static fr.eql.aicap.annuaire.Main.TXTFILE;
 
 
 public class Window extends Application {
@@ -27,11 +36,32 @@ public class Window extends Application {
     //temporary table
     TableView<Stagiaire> tempTable;
 
+    private void Refresh_List(BinaryTree binaryTree,TableView<Stagiaire> table)
+    {
+        List<Stagiaire> data = Stagiaire.Trainees_List(BINARYFILE,binaryTree);
+        ObservableList<Stagiaire> _Trainees_List = FXCollections.observableArrayList(data);
+        table.setItems(_Trainees_List);
+        table.refresh();
+    }
+    private void Btn_Stagiaire_Add_Click(ActionEvent event, BinaryTree binaryTree, TableView<Stagiaire> table)
+    {
+        //Affichage PopUp Stagiaire
 
+        //Code ajout stagiaire
+        Stagiaire julie = new Stagiaire("ai cap1", "2023", "AAA", "julie", "92");
+        julie.Add(BINARYFILE,binaryTree);
+        Refresh_List(binaryTree,table);
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
 
+        Bin_File Bin_File = new Bin_File();
+        BinaryTree binaryTree =  Bin_File.From_Txt_To_Bin(TXTFILE, BINARYFILE);
+        Bin_File.Add_Children_Addresses_into_Parent_Data(BINARYFILE,binaryTree);
+
+        List<Stagiaire> data = Stagiaire.Trainees_List(BINARYFILE,binaryTree);
+        ObservableList<Stagiaire> Trainees_List = FXCollections.observableArrayList(data);
 
         // ObservableList<Stagiaires> data = getStagiairesList();
 
@@ -49,52 +79,119 @@ public class Window extends Application {
         promoCol.setCellValueFactory(
                 new PropertyValueFactory<Stagiaire, String>("promo"));
 
-        TableColumn<Stagiaire, Integer> anneeCol =
-                new TableColumn<Stagiaire, Integer>("Année");
+        TableColumn<Stagiaire, String> anneeCol =
+                new TableColumn<Stagiaire, String>("Année");
         anneeCol.setMinWidth(100);
         anneeCol.setCellValueFactory(
-                new PropertyValueFactory<Stagiaire, Integer>("Année")
+                new PropertyValueFactory<Stagiaire, String>("annee")
         );
 
         TableColumn<Stagiaire, String> nomCol =
                 new TableColumn<Stagiaire, String>("Nom");
         nomCol.setMinWidth(100);
         nomCol.setCellValueFactory(
-                new PropertyValueFactory<Stagiaire, String>("Nom")
+                new PropertyValueFactory<Stagiaire, String>("nom")
         );
 
         TableColumn<Stagiaire, String> prenomCol =
                 new TableColumn<Stagiaire, String>("Prénom");
         prenomCol.setMinWidth(100);
         prenomCol.setCellValueFactory(
-                new PropertyValueFactory<Stagiaire, String>("Prénom")
+                new PropertyValueFactory<Stagiaire, String>("prenom")
         );
 
-        TableColumn<Stagiaire, Integer> dptCol =
-                new TableColumn<Stagiaire, Integer>("Département");
+        TableColumn<Stagiaire, String> dptCol =
+                new TableColumn<Stagiaire, String>("Département");
         dptCol.setMinWidth(100);
         dptCol.setCellValueFactory(
-                new PropertyValueFactory<Stagiaire, Integer>("Département")
+                new PropertyValueFactory<Stagiaire, String>("dpt")
         );
+
 
 
         // ajout des colonnes à la table
 
-        table.getColumns().addAll(promoCol,nomCol,prenomCol, anneeCol,dptCol);
-        // table.setItems(data);
+        table.getColumns().addAll(promoCol, nomCol, prenomCol, anneeCol, dptCol);
+        table.setItems(Trainees_List);
 
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_LEFT);
-        Button button1= new Button("Ajouter un stagiaire");
-        Button button2= new Button("Exporter en PDF");
+        Button buttonAdd = new Button("Ajouter un stagiaire");
+        Button buttonExport = new Button("Exporter en PDF");
 
         //go to scene Rechercher
-        Button button3= new Button("Rechercher");
-        button3.setOnAction(e -> stage.setScene(sceneSearch));
+        Button buttonSearch = new Button("Rechercher");
+        buttonSearch.setOnAction(e -> stage.setScene(sceneSearch));
 
-        Button button4= new Button("Se connecter");
+        Button buttonConnexion = new Button("Se connecter");
 
-        hbBtn.getChildren().addAll(button1, button2, button3, button4);
+        hbBtn.getChildren().addAll(buttonAdd, buttonExport, buttonSearch, buttonConnexion);
+
+
+        //action bouton se connecter
+        buttonAdd.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Btn_Stagiaire_Add_Click(event,binaryTree,table);
+            }
+        });
+
+        buttonConnexion.setOnAction(new EventHandler<ActionEvent>() {
+            TextField dataLogin = new TextField();
+            @Override
+            public void handle(ActionEvent event) {
+                GridPane windowCoGrille = new GridPane();
+                Scene windowCoScene = new Scene(windowCoGrille, 500, 300);
+                // Nouvelle Fenêtre (Stage)
+                Stage windowCo = new Stage();
+                windowCo.setTitle("Fenêtre de connexion");
+                windowCo.setScene(windowCoScene);
+
+                // GridPane Layout
+                windowCoGrille.setAlignment(Pos.CENTER);
+                windowCoGrille.setHgap(10);
+                windowCoGrille.setVgap(10);
+                windowCoGrille.setPadding(new Insets(20, 20, 20, 20));
+
+                //Remplir la grille
+                Text titre = new Text("Connectez-vous");
+                titre.setId("titreText"); //noeud pour CSS
+                windowCoGrille.add(titre, 1, 0, 2, 1);
+
+                Label labelLogin = new Label("Login");
+                windowCoGrille.add(labelLogin, 0, 1);
+                TextField textLogin = new TextField();
+                windowCoGrille.add(textLogin, 1, 1);
+
+                Label labelPassword = new Label("Password");
+                windowCoGrille.add(labelPassword, 0, 2);
+                TextField textPassword = new TextField();
+                windowCoGrille.add(textPassword, 1, 2);
+
+                //Nouveau bouton
+                Button btnCo = new Button("Connexion");
+                windowCoGrille.add(btnCo, 1, 3);
+//                btnCo.setOnAction(new EventHandler<ActionEvent>() {
+//                    @Override
+//                    public void handle(ActionEvent event) {
+//                        WindowAdmin windowUserAdmin = new WindowAdmin();
+//                        Scene sceneAdmin = new Scene(windowUserAdmin.(), 800, 500);
+//                        Stage stageAdmin = new Stage();
+//                        stageAdmin.setScene(sceneAdmin);
+//                        stageAdmin.show();
+//                    }
+//                });
+
+                // Définir la position de la nouvelle fenetre
+                //relativement à la fenetre principale.
+                windowCo.setX(stage.getX() + 200);
+                windowCo.setY(stage.getY() + 100);
+                //Affichage de la nouvelle fenêtre
+                windowCo.show();
+                windowCoScene.getStylesheets().add(getClass().getResource("css.css").toExternalForm());
+            }
+        });
+
 
 
         // RECHERCHER --------------------------------------------------------
