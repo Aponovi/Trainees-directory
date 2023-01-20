fb5477d8e073fe64af4a4f725ef3831813f95b0b
Aponovi <elfi.krupsky@gmail.com>
2023-01-20 15:26:17 +0100

    remove a comment

--- src/fr/eql/aicap/annuaire/Main.java
+++ src/fr/eql/aicap/annuaire/Main.java
@@ -7,10 +7,8 @@ import javafx.application.Application;
 
 public class Main {
     private static final String FOLDER = "files/";
-    private static final String RAF = "stagiaires.bin";
     public static final String BINARYFILE = "files/stagiaires.bin";
     public static final String TXTFILE = "files/stagiaires.txt";
-    //public static BinaryTree binaryTree = null;
 
     public static void main(String[] args) {
         File folder = new File(FOLDER);
